8707de02bbdb5a54d2a3867595ea1bf8219273b3
beatrizbussolo <beatriz.bussolo@escola.pr.gov.br>
2025-05-23 08:10:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'Qual é o valor exato da gravidade terrestre?',
+    '9.807'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem foi D. João VI?',
+    'Rei de Portugal'
+)
+
+criaCartao(
+    'História',
+    'Qual a base do sistema capitalista?',
+    ' O lucro'
+)
+
+criaCartao(
+    'Química',
+    'A passagem da água no estado sólido para o estado líquido é chamada de?',
+    'Fusão'
+)
